59b7ce71b95825a16f248961e1cac1e3fad66e0a
stronray <stronray@gmail.com>
2014-05-14 03:28:29 +0000

    create the user api key when the user is created

--- api/app/controllers/spree/api/users_controller.rb
+++ api/app/controllers/spree/api/users_controller.rb
@@ -18,6 +18,7 @@ module Spree
         authorize! :create, Spree.user_class
         @user = Spree.user_class.new(user_params)
         if @user.save
+	  @user.generate_spree_api_key!
           respond_with(@user, :status => 201, :default_template => :show)
         else
           invalid_resource!(@user)
